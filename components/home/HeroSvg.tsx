7c4a068a692fbf2f05e4dba8efe1cd870b95a743
touraye <touraye07@gmail.com>
2025-09-19 19:01:09 +0000

    add svg line

---
+++ components/home/HeroSvg.tsx
@@ -0,0 +1,481 @@
+export default function HeroSVG() {
+	return (
+		<div className='animation absolute inset-0'>
+			<div className=' '>
+				<svg
+					width='766'
+					height='486'
+					viewBox='0 0 766 486'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'>
+					<g clip-path='url(#clip0_3694_1191)'>
+						<g opacity='0.67'>
+							<line
+								opacity='0.2'
+								x1='246.5'
+								y1='-2.18557e-08'
+								x2='246.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='319.5'
+								y1='1.45007e-08'
+								x2='319.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='392.5'
+								y1='1.45007e-08'
+								x2='392.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='465.5'
+								y1='1.45007e-08'
+								x2='465.5'
+								y2='484'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='538.5'
+								y1='1.45007e-08'
+								x2='538.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='611.5'
+								y1='1.45007e-08'
+								x2='611.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='684.5'
+								y1='1.45007e-08'
+								x2='684.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+						</g>
+						<g opacity='0.67'>
+							<line
+								opacity='0.2'
+								x1='766'
+								y1='119.5'
+								x2='-4.1046e-08'
+								y2='119.5'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='766'
+								y1='192.5'
+								x2='-208'
+								y2='192.5'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='766'
+								y1='265.5'
+								x2='-208'
+								y2='265.5'
+								stroke='#FFD32B'></line>
+						</g>
+						<path
+							opacity='0.5'
+							d='M217 192.5H147.5L147.425 119H-248'
+							stroke='url(#paint3_linear_3694_1191)'></path>
+						<path
+							opacity='0.5'
+							d='M389.5 413H320V485.5H-75'
+							stroke='url(#paint4_linear_3694_1191)'></path>
+						<path
+							opacity='0.5'
+							d='M244.5 265H175V337.5H-220'
+							stroke='url(#paint5_linear_3694_1191)'></path>
+						<g className='animation_bgSvgDotGrp__IRXMQ'>
+							<rect
+								className='animation_horizontalmovesetFour__Brp20'
+								x='244'
+								y='263'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmovesetThree__xPy_K'
+								x='7'
+								y='263'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmovesetTwo__M4_pq'
+								x='76'
+								y='263'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmove__oSbpW'
+								x='81'
+								y='190'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className=' animate-[animation_horizontalmovesetFour_Brp20_4s_linear_infinite]'
+								x='244'
+								y='117'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmovesetThree__xPy_K'
+								x='145'
+								y='190'
+								width='5'
+								height='5'
+								fill='#6FC469'></rect>
+							<rect
+								className='animation_horizontalmovesetFour__Brp20'
+								x='33'
+								y='190'
+								width='5'
+								height='5'
+								fill='#6FC469'></rect>
+						</g>
+						<g className='animation_bgSvglineGrp__Dqhde'>
+							<path
+								className='animation_verticallinemove__A_4LI'
+								d='M466 -32L466 82'
+								stroke='url(#paint6_linear_3694_1191)'></path>
+							<path
+								className='animation_Horizontallinemove__miImK'
+								d='M208 118L322 118'
+								stroke='url(#paint0_linear_3694_1191)'></path>
+							<path
+								className='animation_Horizontallinemove__miImK'
+								d='M424 265L538 265'
+								stroke='url(#paint2_linear_3694_1191)'></path>
+						</g>
+					</g>
+					<defs>
+						<linearGradient
+							id='paint0_linear_3694_1191'
+							x1='322'
+							y1='117.5'
+							x2='208'
+							y2='117.5'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint1_linear_3694_1191'
+							x1='200'
+							y1='191.5'
+							x2='86'
+							y2='191.5'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint2_linear_3694_1191'
+							x1='538'
+							y1='264.5'
+							x2='424'
+							y2='264.5'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint3_linear_3694_1191'
+							x1='217'
+							y1='156'
+							x2='-248'
+							y2='156'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#6FC469'></stop>
+							<stop offset='1' stop-color='#181E25'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint4_linear_3694_1191'
+							x1='389.5'
+							y1='449.25'
+							x2='-75'
+							y2='449.25'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#6FC469'></stop>
+							<stop offset='1' stop-color='#181E25'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint5_linear_3694_1191'
+							x1='244.5'
+							y1='301.25'
+							x2='-220'
+							y2='301.25'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#6FC469'></stop>
+							<stop offset='1' stop-color='#181E25'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint6_linear_3694_1191'
+							x1='465.5'
+							y1='82'
+							x2='465.5'
+							y2='-32'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<clipPath id='clip0_3694_1191'>
+							<rect width='766' height='486' fill='white'></rect>
+						</clipPath>
+					</defs>
+				</svg>
+			</div>
+			<div className=' animation_svgFlip__hIW0_'>
+				<svg
+					width='766'
+					height='486'
+					viewBox='0 0 766 486'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'>
+					<g clip-path='url(#clip0_3694_1191)'>
+						<g opacity='0.67'>
+							<line
+								opacity='0.2'
+								x1='246.5'
+								y1='-2.18557e-08'
+								x2='246.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='319.5'
+								y1='1.45007e-08'
+								x2='319.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='392.5'
+								y1='1.45007e-08'
+								x2='392.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='465.5'
+								y1='1.45007e-08'
+								x2='465.5'
+								y2='484'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='538.5'
+								y1='1.45007e-08'
+								x2='538.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='611.5'
+								y1='1.45007e-08'
+								x2='611.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='684.5'
+								y1='1.45007e-08'
+								x2='684.5'
+								y2='485'
+								stroke='#FFD32B'></line>
+						</g>
+						<g opacity='0.67'>
+							<line
+								opacity='0.2'
+								x1='766'
+								y1='119.5'
+								x2='-4.1046e-08'
+								y2='119.5'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='766'
+								y1='192.5'
+								x2='-208'
+								y2='192.5'
+								stroke='#FFD32B'></line>
+							<line
+								opacity='0.2'
+								x1='766'
+								y1='265.5'
+								x2='-208'
+								y2='265.5'
+								stroke='#FFD32B'></line>
+						</g>
+						<path
+							opacity='0.5'
+							d='M217 192.5H147.5L147.425 119H-248'
+							stroke='url(#paint3_linear_3694_1191)'></path>
+						<path
+							opacity='0.5'
+							d='M389.5 413H320V485.5H-75'
+							stroke='url(#paint4_linear_3694_1191)'></path>
+						<path
+							opacity='0.5'
+							d='M244.5 265H175V337.5H-220'
+							stroke='url(#paint5_linear_3694_1191)'></path>
+						<g className='animation_bgSvgDotGrp__IRXMQ'>
+							<rect
+								className='animation_horizontalmovesetFour__Brp20'
+								x='244'
+								y='263'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmovesetThree__xPy_K'
+								x='7'
+								y='263'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmovesetTwo__M4_pq'
+								x='76'
+								y='263'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmove__oSbpW'
+								x='81'
+								y='190'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmove__oSbpW'
+								x='244'
+								y='117'
+								width='5'
+								height='5'
+								fill='#FFD32B'></rect>
+							<rect
+								className='animation_horizontalmovesetThree__xPy_K'
+								x='145'
+								y='190'
+								width='5'
+								height='5'
+								fill='#6FC469'></rect>
+							<rect
+								className='animation_horizontalmovesetFour__Brp20'
+								x='33'
+								y='190'
+								width='5'
+								height='5'
+								fill='#6FC469'></rect>
+						</g>
+						<g className='animation_bgSvglineGrp__Dqhde'>
+							<path
+								className='animation_verticallinemove__A_4LI'
+								d='M466 -32L466 82'
+								stroke='url(#paint6_linear_3694_1191)'></path>
+							<path
+								className='animation_Horizontallinemove__miImK'
+								d='M208 118L322 118'
+								stroke='url(#paint0_linear_3694_1191)'></path>
+							<path
+								className='animation_Horizontallinemove__miImK'
+								d='M424 265L538 265'
+								stroke='url(#paint2_linear_3694_1191)'></path>
+						</g>
+					</g>
+					<defs>
+						<linearGradient
+							id='paint0_linear_3694_1191'
+							x1='322'
+							y1='117.5'
+							x2='208'
+							y2='117.5'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint1_linear_3694_1191'
+							x1='200'
+							y1='191.5'
+							x2='86'
+							y2='191.5'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint2_linear_3694_1191'
+							x1='538'
+							y1='264.5'
+							x2='424'
+							y2='264.5'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint3_linear_3694_1191'
+							x1='217'
+							y1='156'
+							x2='-248'
+							y2='156'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#6FC469'></stop>
+							<stop offset='1' stop-color='#181E25'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint4_linear_3694_1191'
+							x1='389.5'
+							y1='449.25'
+							x2='-75'
+							y2='449.25'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#6FC469'></stop>
+							<stop offset='1' stop-color='#181E25'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint5_linear_3694_1191'
+							x1='244.5'
+							y1='301.25'
+							x2='-220'
+							y2='301.25'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#6FC469'></stop>
+							<stop offset='1' stop-color='#181E25'></stop>
+						</linearGradient>
+						<linearGradient
+							id='paint6_linear_3694_1191'
+							x1='465.5'
+							y1='82'
+							x2='465.5'
+							y2='-32'
+							gradientUnits='userSpaceOnUse'>
+							<stop stop-color='#FFDC55'></stop>
+							<stop offset='1' stop-color='#FFDC55' stop-opacity='0'></stop>
+						</linearGradient>
+						<clipPath id='clip0_3694_1191'>
+							<rect width='766' height='486' fill='white'></rect>
+						</clipPath>
+					</defs>
+				</svg>
+			</div>
+		</div>
+	)
+}
+
