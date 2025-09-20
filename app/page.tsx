241db089780d6e1370fc3d5e50995e9a749f129d
touraye <touraye07@gmail.com>
2025-09-20 14:32:23 +0000

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <CompanyHero />
 
       {/* The next generation  */}
-      <NextGeneration />
+      <NextGeneration />      
 
       {/* our impact */}
       <Impact />
