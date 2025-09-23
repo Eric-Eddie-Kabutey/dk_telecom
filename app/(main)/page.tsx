f33e071690bdc4b573ab3586ef1c467c1007661e
touraye <touraye07@gmail.com>
2025-09-23 10:36:02 +0000

    update: add new clients logo

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,13 +5,13 @@ import dklogo from "@/public/assets/images/dk_logo.png"
 import Image from "next/image";
 import { CompanyHero } from "@/components/home/Hero";
 import { NextGeneration } from "@/components/home/NextGeneration";
-import { OurClients } from "@/components/home/OurClients";
 import { CaseStudySlider } from "@/components/home/CaseStudySlider";
 import { SolvingProblems } from "@/components/home/SolvingProblems";
 import { TestimonialSliderSection } from "@/components/home/TestimonialSection";
 import { CtaSection } from "@/components/home/CtaSection";
 import { SolutionsOrbitSection } from "@/components/home/SolutionsOrbitSection";
 import { NewImpact } from "@/components/home/NewImpact";
+import { ClientsLogo } from "@/components/home/ClientsLogo";
 
 
 export default function Home() {
@@ -35,8 +35,8 @@ export default function Home() {
       {/* new impact */}
       <NewImpact />
 
-      {/* our clients */}
-      <OurClients />
+      {/* clients logo */}    
+      <ClientsLogo />
 
       {/* case studies slider */}
       <CaseStudySlider />
